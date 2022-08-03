c48d4079f051157fff3fba11674abb282dd4b00c
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-08-03 18:35:57 +0530

    Add files via upload

---
+++ 15_pair_sum_problem.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+bool pairsum(int a[], int n, int k)
+{
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=1; j<n; j++)
+        {
+            if(a[i]+a[j]==k)
+            {
+                cout<<i<<" "<<j<<endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int k;
+    cout<<"enter the sum which you want to search";
+    cin>>k;
+
+    cout<<pairsum(a ,n,k);
+    return 0;
+}
