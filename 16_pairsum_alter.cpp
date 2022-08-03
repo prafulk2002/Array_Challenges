510a7016500596cde56ee837417e6cb6ef6f85ad
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-08-03 18:36:16 +0530

    Add files via upload

---
+++ 16_pairsum_alter.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+bool pairsum(int a[], int n, int k)
+{
+    int low=0;
+    int high=n-1;
+    while(low<high)
+    {
+        if(a[low]+a[high]==k)
+        {
+            cout<<low<<" "<<high<<endl;
+            return true;
+        }
+        else if(a[low]+a[high]>k)
+        {
+            high--;
+        }
+        else{
+            low++;
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int k;
+    cout<<"Enter tha sum you want to search";
+    cin>>k;
+
+    cout<<pairsum(a,n,k);
+    return 0;
+
+}
